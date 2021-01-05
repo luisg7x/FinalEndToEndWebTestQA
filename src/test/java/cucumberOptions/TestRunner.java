c4337ec71fd7d9caf2cfcc16031f49bb321b1f0b
Luis Gabriel <luisg7@outlook.com>
2021-01-04 19:29:47 -0600

    Disabling TestRunner Cucumber for only selenium and rest testing

--- src/test/java/cucumberOptions/TestRunner.java
+++ src/test/java/cucumberOptions/TestRunner.java
@@ -4,11 +4,11 @@ import org.junit.runner.RunWith;
 
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
-
+/*
 @RunWith(Cucumber.class)
 @CucumberOptions(
 		features = "src/test/java/features",
-		glue = "stepDefinitions")
+		glue = "stepDefinitions")*/
 public class TestRunner {
 
 }
